766ebe838848f3dfb49abfc7e2ec10799ad300c5
Lais Vang <vang.lais05@gmail.com>
2018-07-24 16:05:13 -0500

    finsihed base requirements

--- client.js
+++ client.js
@@ -17,4 +17,48 @@ const employees = [ atticus, jem, scout, robert, mayella ];
 
 // YOU SHOULD NOT NEED TO CHANGE ANYTHING ABOVE THIS POINT
 
-console.log( employees );
+console.table( employees );
+
+let employeeBonuses = [];
+for (let employee of employees){
+  let employeeBonus = calculateBonus(employee);
+  employeeBonuses.push(employeeBonus)
+  console.table(employeeBonus);
+}
+
+function calculateBonus(employee){
+  let rating = employee.reviewRating;
+  let bonusPercentage = 0;
+  if (rating === 3){
+    bonusPercentage = 0.04;
+  }
+  else if (rating === 4){
+    bonusPercentage = 0.06;
+  }
+  else if (rating === 5){
+    bonusPercentage = 0.10;
+  }
+  if(employee.employeeNumber.length === 4){
+    bonusPercentage += 0.05;
+  }
+  if(parseInt(employee.annualSalary) > 65000){
+    bonusPercentage -= 0.01;
+  }
+  if (bonusPercentage > 0.13) {
+    bonusPercentage = 0.13;
+  }
+  else if (bonusPercentage < 0){
+    bonusPercentage = 0;
+  }
+
+  let bonusAmount = Math.round(employee.annualSalary * bonusPercentage);
+  let employeeBonusCalculation = {
+    name: employee.name,
+    bonusPercentage: bonusPercentage,
+    totalCompensation: parseInt(employee.annualSalary) + bonusAmount,
+    totalBonus: bonusAmount
+  }
+  return employeeBonusCalculation;
+} // end calculateBonus
+
+//console.log("Atticus bonus should be 0.09: Bonus returned was:", employeeBonuses[0].bonusPercentage);
